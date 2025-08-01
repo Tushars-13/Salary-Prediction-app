f230b37efac0992a9189e0468208ec6a8c024503
Tushar Shukla <shuklatushar7399@gmail.com>
2025-08-01 20:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("💼 Salary Prediction App")
 
 education = st.selectbox("Education Level", [0, 1, 2], format_func=lambda x: ["High School", "Bachelor's", "Master's/PhD"][x])
 experience = st.number_input("Years of Experience", 0, 40, step=1)
-location = st.selectbox("Location", [0, 1, 2], format_func=lambda x: ["City A", "City B", "City C"][x])
+location = st.selectbox("Location", [0, 1, 2], format_func=lambda x: ["Rural", "Suburban", "Urban"][x])
 job_title = st.selectbox("Job Title", [0, 1, 2], format_func=lambda x: ["Developer", "Manager", "Analyst"][x])
 age = st.number_input("Age", 18, 70, step=1)
 gender = st.selectbox("Gender", [0, 1], format_func=lambda x: ["Male", "Female"][x])
